4324be5543fd0063f16e021afd1d88c74812533f
genixiong <genixiong0630@hotmail.com>
2019-11-19 13:06:16 -0500

    splash screen
    disable ic_launcher_background, enable splash screen with a logo picture

--- app/src/main/java/com/cs528/parkingassist/Activity/MainActivity.java
+++ app/src/main/java/com/cs528/parkingassist/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cs528.parkingassist.R;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("start", "MainActivity");
 
     }
 }
--- app/src/main/java/com/cs528/parkingassist/Activity/StartActivity.java
+++ app/src/main/java/com/cs528/parkingassist/Activity/StartActivity.java
@@ -4,17 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.os.Handler;
 
 import com.cs528.parkingassist.R;
 
 public class StartActivity extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        Log.i("start", "StartActivity");
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
     }
 }
